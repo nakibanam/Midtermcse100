d7a48140a4bb9806e0df22c01af9df6f86e22696
Nakib Anam <mkanakib@hotmail.com>
2017-03-08 13:55:47 +0600

    Q2

---
+++ Q2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+    // declaring a string variable to store the name into a variable called name;
+string name;
+
+cout << "What is your name?" << endl;
+cin >> name;
+
+    // declaring an integer variable for age
+
+int age;
+
+cout << "What is your age?" << endl;
+cin >> age;
+// declaring an int variable for phone number
+
+int no;
+
+cout << "What is your mobile number?" << endl;
+cin >> no;
+
+cout <<  "Name: " << name << endl;
+cout <<  "Age: " << age << endl;
+cout <<  "Mobile number: 0" << no << endl;
+return 0;
+}
+
