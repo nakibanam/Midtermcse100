164de21fae1dc0f7056a1650c6fa513d597b7337
Nakib Anam <mkanakib@hotmail.com>
2017-03-08 14:03:45 +0600

    Q3

---
+++ Q3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // declaring a string variable for storing a symbol
+ string symbol;
+ cout << "Please enter the character of which you want to print the F shape :" <<endl;
+ cin >> symbol;
+
+ cout <<symbol<<symbol<<symbol<<symbol<<symbol<<endl;
+
+ cout <<symbol<<endl;
+
+ cout <<symbol<<endl;
+
+ cout <<symbol<<symbol<<symbol<<symbol<<endl;
+
+ cout <<symbol<<endl;
+
+ cout <<symbol<<endl;
+
+ cout <<symbol<<endl;
+
+ return 0;
+
+}
