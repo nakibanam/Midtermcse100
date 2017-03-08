448ee5d7f839c8ae7684d45a67bdb41f1df93e22
Nakib Anam <mkanakib@hotmail.com>
2017-03-08 14:48:42 +0600

    Q10

---
+++ Q10.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+ // declaring strings for name and surname
+
+ string name;
+ string surname;
+
+ //declaring a integer variable for birth year
+
+ int year;
+
+ cout << "Enter your first name: " << endl;
+ cin >> name;
+ cout << "Enter your first name: " << endl;
+ cin >> surname;
+ cout << "Enter your birth year: " << endl;
+ cin >>year;
+
+ cout << name <<" " << surname << " " << year << endl;
+
+ return 0;
+
+
+}
+
